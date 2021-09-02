a4a59cbc0c7e89f4998cbacb0c782c7e59cfbb2d
Airidc <airidas.venskus@gmail.com>
2021-09-02 23:50:47 +0200

    some work.

--- lib/store/slices/authSlice.ts
+++ lib/store/slices/authSlice.ts
@@ -10,6 +10,7 @@ import { UserDTO } from "../../DTO/userDTO";
 import { User } from "../../types/User";
 import { RootState } from "../store";
 import { appConfigSlice } from "./appConfigSlice";
+import Router from "next/router";
 
 export enum AuthStates {
   idle = "Idle",
@@ -35,7 +36,17 @@ const initialState = {
   refreshToken: "",
   refreshTokenExpiry: 0,
   loading: AuthStates.idle,
-  user: undefined,
+  user: <User>{
+    id: "b73c4d26-ce6c-4d6f-9f8d-1d90355c4567",
+    username: "Airidc69",
+    email: "test33s@tes2.test21",
+    createdAt: "1626726718215",
+    userRole: "user",
+    profilePicPath: "",
+    firstName: "",
+    lastName: "",
+    currency: "EUR",
+  },
 };
 
 export const login = createAsyncThunk(
@@ -79,6 +90,7 @@ export const authSlice = createSlice({
       state.refreshToken = action.payload.refreshToken;
       state.refreshTokenExpiry = action.payload.refreshTokenExpiry;
       state.loading = AuthStates.idle;
+      Router.push("/dashboard");
     });
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
+import { Provider, useStore } from 'react-redux'
 import { store, wrapper } from '../lib/store/store'
 import Layout from '../components/layout'
+import { AuthSliceState, selectUserData } from '../lib/store/slices/authSlice'
+import { useAppSelector } from '../lib/store/hooks'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const authData = useAppSelector<AuthSliceState>(selectUserData);
 
   return (
     <Provider store={store}>
-      <Layout>
+      {authData.user ? <Layout>
         <Component {...pageProps} />
-      </Layout>
+      </Layout> : <Component {...pageProps} />}
+
     </Provider>)
 }
 export default wrapper.withRedux(MyApp)
